9610cc1fdf582fea00c19d58bdb069b4a99fb1e0
danielsza <daniel@szajkowski.ca>
2025-11-05 12:41:14 -0500

    Update const.py

--- custom_components/wemo_heater/const.py
+++ custom_components/wemo_heater/const.py
@@ -1,8 +1,3 @@
-"""Constants for the Belkin Wemo component."""
+"""Constants for the WeMo Heater integration."""
 
-DOMAIN = "wemo"
-
-SERVICE_SET_HUMIDITY = "set_humidity"
-SERVICE_RESET_FILTER_LIFE = "reset_filter_life"
-
-WEMO_SUBSCRIPTION_EVENT = f"{DOMAIN}_subscription_event"
+DOMAIN = "wemo_heater"
